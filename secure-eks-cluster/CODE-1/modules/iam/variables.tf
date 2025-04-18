93b0da0cf39b9b09ab3cefd72a5d167e072bf940
Lokak07 <lokashakthi19.ls@gmail.com>
2025-04-18 11:47:20 +0000

    removing old code

--- secure-eks-cluster/CODE-1/modules/iam/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "eks_role_name" {
-  description = "The name of the IAM role for the EKS cluster"
-  type        = string
-}
-
-variable "eks_policy_name" {
-  description = "The name of the IAM policy for the EKS cluster"
-  type        = string
-}
